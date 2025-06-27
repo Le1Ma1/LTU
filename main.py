a1cfc3d137954a07442fb6f3e5970f3912028b1c
LeiMaiInLTU <mike6453andy3181@gmail.com>
2025-06-27 12:05:50 +0800

    結構更新

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 import random
 
 app = Flask(__name__)
 app.secret_key = "#230dec61-fee8-4ef2-a791-36f9e680c9fc"
+bootstrap = Bootstrap(app=app)
 
+@app.route('/test')  
+def test():  
+    a= 'Do whatever u want'
+    return render_template('test.html', username=a) 
+
+@app.route('/login')  
+def login():  
+    a= 'Here is Login'
+    return render_template('login.html', username=a)  
+  
+  
+@app.route('/logout')  
+def logout():      
+    b= 'Here is Logout'
+    return render_template('logout.html', username=b)   
+  
+  
+@app.route('/userinfo')  
+def userinfo():     
+    c= 'Here is UserINFO'
+    return render_template('userinfo.html', username=c)
+
+# ----主頁多互動區域----
 @app.route("/", methods=["GET", "POST"])
 def index():
+    rand_num = None
     rand1 = session.get("rand1")
     rand2 = session.get("rand2")
     answer = None
@@ -27,21 +52,11 @@ def index():
 
     return render_template(
         "index.html",
-        rand_num=rand_num
+        rand_num = rand_num,
         rand1=rand1,
         rand2=rand2,
         answer=answer
     )
-
-@app.route('/login')  
-def login():  
-    a= 'Here is Login'
-    return render_template('login.html', username=a)  
-
-@app.route('/user')  
-def userinfo():     
-    c= 'Here is UserINFO'
-    return render_template('userinfo.html', username=c) 
 
 if __name__ == "__main__":
     app.debug = True
